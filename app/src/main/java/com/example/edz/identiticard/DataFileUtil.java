299866ba4984ced7b29c0f077d11d09ac095a2e4
liuyang1 <yang.liua@enwaysoft.com>
2019-10-17 14:07:57 +0800

    qq

--- app/src/main/java/com/example/edz/identiticard/DataFileUtil.java
+++ app/src/main/java/com/example/edz/identiticard/DataFileUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import java.io.File;
 
+//liuyang
 public class DataFileUtil {
     public static File getSaveFile(Context context) {
         return new File(context.getFilesDir(), "pic.jpg");
